f1bfbd9418a90d82b11b7704d1e42de083b51cb9
Nick Maltbie <nick.dmalt@gmail.com>
2024-05-25 18:58:12 -0700

    Fixed more formatting errors

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -1,3 +1,21 @@
+// Copyright (C) 2024 Nicholas Maltbie, Sam Scherer
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -61,7 +79,7 @@ namespace ProjectCoda.Player
         /// <summary>
         /// Radius of the overlap circle to determine if grounded
         /// </summary>
-        const float k_GroundedRadius = .2f;
+        private const float k_GroundedRadius = .2f;
 
         /// <summary>
         /// Whether or not the player is grounded.
@@ -71,7 +89,7 @@ namespace ProjectCoda.Player
         /// <summary>
         /// Radius of the overlap circle to determine if the player can stand up
         /// </summary>
-        const float k_CeilingRadius = .2f;
+        private const float k_CeilingRadius = .2f;
 
         private Rigidbody2D m_Rigidbody2D;
 
@@ -93,18 +111,22 @@ namespace ProjectCoda.Player
         public BoolEvent OnCrouchEvent;
         private bool m_wasCrouching = false;
 
-        private void Awake()
+        public void Awake()
         {
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
 
             if (OnLandEvent == null)
+            {
                 OnLandEvent = new UnityEvent();
+            }
 
             if (OnCrouchEvent == null)
+            {
                 OnCrouchEvent = new BoolEvent();
+            }
         }
 
-        private void FixedUpdate()
+        public void FixedUpdate()
         {
             bool wasGrounded = m_Grounded;
             m_Grounded = false;
@@ -155,12 +177,17 @@ namespace ProjectCoda.Player
 
                     // Disable one of the colliders when crouching
                     if (m_CrouchDisableCollider != null)
+                    {
                         m_CrouchDisableCollider.enabled = false;
-                } else
+                    }
+                }
+                else
                 {
                     // Enable the collider when not crouching
                     if (m_CrouchDisableCollider != null)
+                    {
                         m_CrouchDisableCollider.enabled = true;
+                    }
 
                     if (m_wasCrouching)
                     {
