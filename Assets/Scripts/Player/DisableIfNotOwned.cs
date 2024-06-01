7ca04ac927e419110e1e391905658fd8a9229615
Samuel Scherer <swiimii962@gmail.com>
2024-06-01 19:01:16 -0400

    Added murder

---
+++ Assets/Scripts/Player/DisableIfNotOwned.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ProjectCoda
+{
+    public class DisableIfNotOwned : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            NetworkBehaviour nm = GetComponentInParent<NetworkBehaviour>();
+            if(!nm.IsOwner)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
